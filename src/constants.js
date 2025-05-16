f95a279947c375da61a13af5db40de99c394cf02
Karthikeyan S <karthyavinash2004@gmail.com>
2025-05-16 19:21:46 +0530

    Force add constants.js

---
+++ src/constants.js
@@ -0,0 +1,57 @@
+
+import SkolarLogo from '../src/assets/Companies/skolar.png';
+import RetechLogo from '../src/assets/Companies/retech.jpg';
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Basketballer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Karthi';
+
+export const experiences = [
+    {
+      role: "Machine Learning Intern",
+      company: "SKOLAR",
+      duration: "Nov 2023 – Jan 2023",
+      logo: SkolarLogo,
+      url: "https://skolar.tech",
+      points: [
+        "Worked with real-world datasets to build and optimize supervised and unsupervised machine learning models.",
+    "Conducted data preprocessing, feature selection, and data visualization using Python libraries such as Pandas, NumPy, and Matplotlib.",
+    "Collaborated with a small agile team to develop predictive models and presented performance metrics (accuracy, F1 score).",
+    "Implemented classification models like Random Forest and Logistic Regression for structured data problems.",
+    "Gained hands-on experience with Scikit-learn pipelines and model evaluation techniques."
+      ],
+    },
+    {
+      role: "Machine Learning Intern",
+      company: "Retech Solutions",
+      duration: "May 2024 – Jun 2024",
+      logo: RetechLogo,
+      url: "https://retechsolutions.in",
+      points: [
+        "Developed simple machine learning models for classification using Python and Scikit-learn.",
+    "Explored libraries such as Matplotlib and Seaborn to visualize data insights effectively.",
+    "Cleaned and structured raw datasets to improve model input quality.",
+    "Participated in code reviews and implemented feedback to enhance model logic and readability.",
+    "Gained exposure to practical workflow of real-world ML tasks including data handling and model tuning."
+      ],
+    },
+  ];
+  
+
+
+
+export const EMAIL_JS_SERVICE_ID = "service_zc7fwut";
+export const EMAIL_JS_TEMPLATE_ID = "template_7f5nyhc";
+export const EMAIL_JS_PUBLIC_KEY = "hGmqB3BpYcOuvMoeE";
